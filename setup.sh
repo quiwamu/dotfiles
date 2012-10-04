23c3d8d894f8b63fafa53cb22beebaf4fc0466d0
quiwamu <quiwamu@gmail.com>
2012-10-04 15:36:12 +0900

    modify git automatic and mv existed file

--- setup.sh
+++ setup.sh
@@ -4,25 +4,29 @@ DOT_FILES=( .vimrc .bashrc .zshrc .inputrc )
 
 for file in ${DOT_FILES[@]}
 do
-  if [ -a $HOME/$file ]; then
-    if [ -L $HOME/$file ]; then
-      echo "既にシンボリックリンクが存在します: $file"
-    elif [ -d $HOME/$file ]; then
-      echo "既にディレクトリが存在します: $file"
-    else
-      echo "既にファイルが存在します: $file"
-    fi
-  else
-    ln -s $HOME/dotfiles/$file $HOME/$file
-    echo "シンボリックリンクを貼りますた: $file"
-  fi
+	if [ -a $HOME/$file ]; then
+		if [ -L $HOME/$file ]; then
+			echo "既にシンボリックリンクが存在します: $file"
+		elif [ -d $HOME/$file ]; then
+			echo "既にディレクトリが存在します: $file"
+		else
+			echo "既にファイルが存在しますので、どけておきます: $file"
+			mv $HOME/$file $HOME/$file.1
+			ln -s $HOME/dotfiles/$file $HOME/$file
+			echo "シンボリックリンクを貼りますた: $file"
+		fi
+	else
+		ln -s $HOME/dotfiles/$file $HOME/$file
+		echo "シンボリックリンクを貼りますた: $file"
+	fi
 done
 
 # for oh-my-zsh
 if [ -d $HOME/.oh-my-zsh ]; then
-  echo "pull oh-my-zsh" 
-  git pull git://github.com/quiwamu/oh-my-zsh.git ~/.oh-my-zsh
+	echo "pull oh-my-zsh" 
+	cd ~/.oh-my-zsh
+	git pull
 else
-  echo "clone oh-my-zsh"
-  git clone git://github.com/quiwamu/oh-my-zsh.git ~/.oh-my-zsh
+	echo "clone oh-my-zsh"
+	git clone git://github.com/quiwamu/oh-my-zsh.git ~/.oh-my-zsh
 fi

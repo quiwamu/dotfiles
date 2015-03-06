b052830a6b6f38fa6c6f36d63857524fe36d6057
quiwamu <quiwamu@gmail.com>
2015-03-06 19:53:37 +0900

    gitのpagerを less -r から less -R に変更

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,6 +4,6 @@ git config --global core.editor vim
 #git config --global --unset core.editor //設定の削除
 git config --global color.ui true
 #git diffなどで改行表示する場合
-git config --global core.pager "less -r"
+git config --global core.pager "less -R"
 git config --global --list
 
